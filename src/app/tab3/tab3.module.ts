a691af1cb7d607656983bf411a5140e603b98316
Samueletec2020 <samuelhenrique_08@hotmail.com>
2020-07-15 17:50:34 -0300

    Revert "calculadora simples/normal"
    This reverts commit e146ae6bc6a398f8634850bd2e0949241bacea21.

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,11 +1,12 @@
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
+import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { Tab3PageRoutingModule } from './tab3-routing.module'
 
 @NgModule({
   imports: [
@@ -13,8 +14,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    RouterModule.forChild([{ path: '', component: Tab3Page }]),
+    Tab3PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab3Page]
 })
-export class Tab1PageModule {}
+export class Tab3PageModule {}
